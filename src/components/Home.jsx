a476ff772c193bb3b37bb1f5574c88f59f3cd060
afaqkhannn <afaqkhan113@gmail.com>
2024-05-01 03:05:49 +0500

    Initial Commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+    <div className='text-4xl'>
+     
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/social.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const social = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default social
