2b01fb819eb1c4a4a9aaf53958573c7dc9011dc2
Afaq Khan <afaqkhan113@gmail.com>
2024-09-07 16:40:51 +0500

    Add President's Instagram to PresidentsMessage

--- src/components/PresidentMessage.js
+++ src/components/PresidentMessage.js
@@ -15,63 +15,71 @@ const PresidentsMessage = () => {
         mt: 25,
       }}
     >
-      <Paper
-        sx={{
-          backgroundColor: "rgba(255, 255, 255, 0.1)",
-          backdropFilter: "blur(10px)",
-          borderRadius: 2,
-          p: 4,
-          maxWidth: "800px",
-          mx: "auto",
-          textAlign: "center",
-          position: "relative",
-          boxShadow: 3,
-        }}
+      <a
+        href="https://www.instagram.com/a.b_moiz/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: "none" }} // Remove underline for the link
       >
-        <Avatar
-          src={presidentImage}
-          alt="President"
+        <Paper
           sx={{
-            height: { xs: 160, sm: 120 },
-            width: { xs: 160, sm: 120 },
-            mb: 2,
-            borderRadius: "50%",
+            backgroundColor: "rgba(255, 255, 255, 0.1)",
+            backdropFilter: "blur(10px)",
+            borderRadius: 2,
+            p: 4,
+            maxWidth: "800px",
             mx: "auto",
-            objectFit: "cover", // Ensures the image covers the Avatar without distortion
-            objectPosition: "center top", // Centers on the top part of the image
+            textAlign: "center",
+            position: "relative",
+            boxShadow: 3,
           }}
-        />
-        <Typography
-          variant="h4"
-          sx={{ fontWeight: "bold", color: "#e0e0e0", mb: 2 }}
         >
-          President's Message
-        </Typography>
-        <Typography
-          variant="body2"
-          sx={{ color: "#ffffff", mb: 2, textAlign: "justify" }}
-        >
-          Greetings, <br />
-          <br />
-          I, Abdul Moiz Kamran, President of CY'MUN'24, extend a warm invitation
-          to you for our inaugural event. This conference promises to be a
-          landmark experience, etching its name in the annals of MUN history.{" "}
-          <br /> We are confident that CY'MUN will be the most eagerly awaited
-          MUN conference in the country. Having you join us would be a
-          tremendous honor. We eagerly anticipate your participation. <br />
-        </Typography>
-        <Box sx={{ mt: 2 }}>
+          <Avatar
+            src={presidentImage}
+            alt="President"
+            sx={{
+              height: { xs: 160, sm: 120 },
+              width: { xs: 160, sm: 120 },
+              mb: 2,
+              borderRadius: "50%",
+              mx: "auto",
+              objectFit: "cover",
+              objectPosition: "center top",
+            }}
+          />
           <Typography
-            variant="h6"
-            sx={{ color: "#ffffff", fontWeight: "bold" }}
+            variant="h4"
+            sx={{ fontWeight: "bold", color: "#e0e0e0", mb: 2 }}
           >
-            Abdul Moiz Kamran
+            President's Message
           </Typography>
-          <Typography variant="body1" sx={{ color: "#ffffff" }}>
-            President CY'MUN'24
+          <Typography
+            variant="body2"
+            sx={{ color: "#ffffff", mb: 2, textAlign: "justify" }}
+          >
+            Greetings, <br />
+            <br />
+            I, Abdul Moiz Kamran, President of CY'MUN'24, extend a warm
+            invitation to you for our inaugural event. This conference promises
+            to be a landmark experience, etching its name in the annals of MUN
+            history. <br /> We are confident that CY'MUN will be the most
+            eagerly awaited MUN conference in the country. Having you join us
+            would be a tremendous honor. We eagerly anticipate your
+            participation. <br />
           </Typography>
-        </Box>
-      </Paper>
+          <Box sx={{ mt: 2 }}>
+            <Typography
+              variant="h6"
+              sx={{ color: "#ffffff", fontWeight: "bold" }}
+            >
+              Abdul Moiz Kamran
+            </Typography>
+            <Typography variant="body1" sx={{ color: "#ffffff" }}>
+              President CY'MUN'24
+            </Typography>
+          </Box>
+        </Paper>
+      </a>
     </Box>
   );
 };
